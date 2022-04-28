8a1548b4274450e5df64b9cbfae58ea6b95cd398
JivinSardine <89859763+JivinSardine@users.noreply.github.com>
2022-04-28 09:20:53 +0530

    u

--- js/Form.js
+++ js/Form.js
@@ -4,16 +4,13 @@ class Form {
     this.playButton = createButton("Play");
     this.titleImg = createImg("./assets/title.png", "game title");
     this.greeting = createElement("h2");
-    
   }
 
   setElementsPosition() {
-    this.titleImg.position(30, 100);
+    this.titleImg.position(120, 100);
     this.input.position(width / 2 - 110, height / 2 - 80);
     this.playButton.position(width / 2 - 90, height / 2 - 20);
     this.greeting.position(width / 2 - 300, height / 2 - 100);
-
-    
   }
 
   setElementsStyle() {
@@ -43,8 +40,7 @@ class Form {
       player.addPlayer();
       player.updateCount(playerCount);
       player.getDistance();
-    })
-    
+    });
   }
 
   display() {
